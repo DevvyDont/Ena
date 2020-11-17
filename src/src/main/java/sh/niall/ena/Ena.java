87d60d5d38e9222782542bee232066b731284d74
Niall <50669114+niallsh@users.noreply.github.com>
2020-11-17 01:17:08 +0000

    Register the stats command, chat command and the services

--- src/src/main/java/sh/niall/ena/Ena.java
+++ src/src/main/java/sh/niall/ena/Ena.java
@@ -1,8 +1,13 @@
 package sh.niall.ena;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import sh.niall.ena.commands.chat.ChatColourCommand;
+import sh.niall.ena.commands.player.StatsCommand;
 import sh.niall.ena.commands.spawn.SetSpawnCommand;
 import sh.niall.ena.commands.spawn.SpawnCommand;
+import sh.niall.ena.services.ChatFormatter;
+import sh.niall.ena.services.PlayedDurationTracking;
+import sh.niall.ena.services.PlayerDeathTracking;
 
 public final class Ena extends JavaPlugin {
 
@@ -12,7 +17,13 @@ public final class Ena extends JavaPlugin {
     public void onEnable() {
         plugin = this;
 
-        // Spawn Commands
+        getServer().getPluginManager().registerEvents(new ChatFormatter(), this);
+        getServer().getPluginManager().registerEvents(new PlayedDurationTracking(), this);
+        getServer().getPluginManager().registerEvents(new PlayerDeathTracking(), this);
+
+        // Commands
+        getCommand("chatcolor").setExecutor(new ChatColourCommand());
+        getCommand("stats").setExecutor(new StatsCommand());
         getCommand("spawn").setExecutor(new SpawnCommand());
         getCommand("setspawn").setExecutor(new SetSpawnCommand());
     }
